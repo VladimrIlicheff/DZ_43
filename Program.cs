dcd51b22eab71e440cb8c75e7f62fdd1ca8341b3
Vladimir <user@MAK.local>
2022-11-05 13:28:44 +0700

    ввод чисел с консоле

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых,
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+Console.WriteLine ("Введите значение b1");
+int numFirst = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите значение b1");
+int numSekond = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите значение b1");
+int numThird= int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите значение b1");
+int numFour= int.Parse(Console.ReadLine()!);
